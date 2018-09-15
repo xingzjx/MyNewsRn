f5a543c955ea66c884cd08e4b27c821274f6f906
xingzjx <1181313247@qq.com>
2018-09-15 23:20:52 +0800

    新闻详情页完成

--- js/news/NewsDetailPage.js
+++ js/news/NewsDetailPage.js
@@ -1,17 +1,73 @@
 import React, {Component} from "react";
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet, Image,WebView,ToastAndroid,Dimensions} from 'react-native';
+import {
+    Header,
+    Container,
+    Title,
+    Body,
+    Icon,
+    Left,
+    Button,
+    Right,
+
+
+} from 'native-base';
+
+const {width, height} = Dimensions.get('window');
 
 export default class NewsDetailPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            url:props.navigation.getParam('url'),
+
+        };
     };
 
     render() {
         return (
-            <View>
-                <Text>news</Text>
-            </View>
+            <Container>
+                <Header>
+                    <Left>
+                        <Button
+                            transparent={true}
+                            onPress={()=>{
+                                this.props.navigation.goBack()
+                            }}>
+                            <Icon name='arrow-back' />
+                        </Button>
+                    </Left>
+                    <Body>
+                    <Title>
+                        新闻详情
+                    </Title>
+                    </Body>
+                </Header>
+                <View style={styles.container}>
+                    <WebView
+                        source={{uri: this.state.url}}
+                        startInLoadingState={true}
+                        domStorageEnabled={true}//开启dom存贮
+                        javaScriptEnabled={true}//开启js
+                        style={styles.webview_style}
+                        onLoad={(e) => console.log('onLoad')}
+                        onLoadEnd={(e) => console.log('onLoadEnd')}
+                        onLoadStart={(e) => console.log('onLoadStart')}>
+                    </WebView>
+                </View>
+            </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    webview_style: {
+        width: width,
+        height: height,
+    }
+});
